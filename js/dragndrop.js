2f9f2ed78475df782cc460d92b21d18bac002358
Orlando J Betancourth <orlando.betancourth@gmail.com>
2020-12-10 21:24:09 -0600

    Drag and Drop

---
+++ js/dragndrop.js
@@ -0,0 +1,42 @@
+/*
+onDrag   drag
+onDragOver  dragover
+onDrop  drop
+*/
+
+document.addEventListener("DOMContentLoaded", function(){
+  var vcards = document.getElementsByClassName("vcard");
+  var ccards = document.getElementsByClassName("acard");
+  for(var index= 0; index < ccards.length; index ++){
+    ccards[index].addEventListener("dragstart", function(e){
+      e.dataTransfer.setData("text", e.target.dataset.value);
+    });
+  }
+  for (var index = 0; index < vcards.length; index++) {
+    vcards[index].addEventListener("dragenter", function (e) {
+      if (/vcard/.test(e.toElement.className)){
+        e.toElement.classList.toggle("over");
+      }
+    },false);
+    vcards[index].addEventListener("dragleave", function (e) {
+      if (/vcard/.test(e.toElement.className)) {
+        e.toElement.classList.toggle("over");
+      }
+    }, false);
+    vcards[index].addEventListener("dragover", function (e) {
+      e.preventDefault();
+    },false);
+    vcards[index].addEventListener("drop", function (e) {
+      e.preventDefault();
+      if (/vcard/.test(e.toElement.className)) {
+        var answer = e.dataTransfer.getData("text");
+        var question = e.target.dataset.answer;
+        if (answer == question) {
+          e.target.classList.toggle("correct");
+        }else{
+          e.target.classList.toggle("over");
+        }
+      }
+    }, false);
+  }
+});
